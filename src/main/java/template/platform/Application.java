a2a00445b5353e5127ba47134118b78dfb62339a
yinshixiao <593853565@qq.com>
2018-08-07 21:40:11 +0800

    aaaaa

--- src/main/java/template/platform/Application.java
+++ src/main/java/template/platform/Application.java
@@ -1,15 +1,44 @@
 package template.platform;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class Application {
+public class Application  {
 	public static void main(String[] args) {  
 		SpringApplication.run(Application.class, args);  
     } 
-	
-	
-	
 }
+
+
+
+/* 需要部署到tomcat时候这么写*/
+/*
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
+
+}  
+*/
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
